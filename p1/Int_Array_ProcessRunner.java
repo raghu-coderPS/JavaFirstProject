24b564db93a36a00910e785a56f9ce5d5d109493
raghu <raghuramkanagarla123@gmail.com>
2020-05-01 13:11:27 +0530

    updated Int_Array_Process file

--- p1/Int_Array_ProcessRunner.java
+++ p1/Int_Array_ProcessRunner.java
@@ -5,18 +5,19 @@ public class Int_Array_ProcessRunner {
 	public static void main(String[] args) {
 		
         //System.out.println(FigToWords.getWords(1234));
-		int[] a= {1,5,7,8,9,2};
+		int[] a= {1,5,7,8,9,2,9};
          Int_Array_Process p = new Int_Array_Process(a);	
          //Int_Array_Process p1 = new Int_Array_Process(p);
          //p.readData();
-         p.sort();
-         p.displayData();
-         p.getSum();
-         p.findingExtremesInArray();
+        // p.sort();
+       //  p.displayData();
+        // p.getSum();
+        // p.findingExtremesInArray();
          //Int_Array_Process p2=p1;
          //System.out.println(p);
          //System.out.println(p1);
          //System.out.println(p2);
+         
 	}
 
 }
