2e44cfde87dafcc3efc3b02c2d7d770ca37811ff
raghu <raghuramkanagarla123@gmail.com>
2020-05-08 13:48:43 +0530

    Adding files

---
+++ com/sapient/students/StudentNotFoundException.java
@@ -0,0 +1,14 @@
+package com.sapient.students;
+
+public class StudentNotFoundException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public StudentNotFoundException(String message) {
+		super(message);
+	}
+
+}
